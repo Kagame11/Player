0c9b8b2d73c66f05bb1dafe473b6f3b6f1136f5c
intwari kagame fred <intwari.k.fred@gmail.com>
2024-03-29 17:42:37 -0700

    referencing values with refs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useState, useRef, useEffect } from 'react';
+import {createConnection} from './chat.js';
+
+function VideoPlayer ({src, isPlaying}) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (isPlaying) {
+      console.log('Calling video.play()');
+      ref.current.play();
+    } else {
+      console.log('Calling video.pause()');
+      ref.current.pause();
+    }
+  }, [isPlaying]);
+
+  return <video ref={ref} src={src} loop playInline />;
+}
+
+function ChatRoom({roomId}) {
+  const [serverUrl, setServerUrl] = useState('https://localhost:1234');
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const connection = createConnection(serverUrl, roomId);
+    connection.connect();
+    return () => {
+      connection.disconnect();
+    };
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <label>
+      Server URL:{''}
+      <input
+      value={serverUrl}
+      onChange={e => setServerUrl(e.target.value)}
+      />
+      </label>
+      <h1>Welcome to the {roomId} room!</h1>
+      <label>
+        Your message:{''}
+        <input value={message} onChange={e => setMessage(e.target.value)} />
+      </label>
+      </>
+      
   );
 }
 
-export default App;
+export default function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [text, setText] = useState('');
+  const [show, setShow] = useState(false);
+  const [roomId, setRoomId] = useState('general');
+  return (
+    <>
+    <input value={text} onChange={e => setText(e.target.value)} />  
+    <button onClick={() => setIsPlaying(!isPlaying)}>
+      {isPlaying ? 'Pause' : 'Play'}
+    </button>
+    <VideoPlayer isPlaying={isPlaying} src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+
+    />
+
+    <label>
+      Choose the chat room:{''}
+      <select
+      value={roomId}
+      onChange={e => setRoomId(e.target.value)}>
+        <option value="general">general</option>
+        <option value="music">music</option>
+        <option value="travel">travel</option>
+      </select>
+      <button onClick={() => setShow(!show)}>
+        {show ? 'Close chat' : 'Open chat'}
+      </button>
+    </label>
+    {show && <hr />}
+    {show && <ChatRoom roomId={roomId}/>}
+    </>
+  );
+}
